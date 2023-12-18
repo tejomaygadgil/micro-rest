a085cc22c9e090443d64479764b9d8c0025143ec
Tejomay Gadgil <tejomaygadgil@gmail.com>
2023-12-18 09:41:56 -0800

    Switch to pactl for media monitoring

--- micro-rest.sh
+++ micro-rest.sh
@@ -14,11 +14,15 @@ while true; do
   sleep $(($loop_time + $random_time))
 
   # See if media is playing
-  media_status=$(playerctl status 2> /dev/null)
+  if pactl list | grep -q RUNNING; then
+      media_status=1
+  else
+      media_status=0
+  fi
 
   # Pause if playing
-  if [ "$media_status" == "Playing" ]; then
-    playerctl pause
+  if [ "$media_status" -eq 1 ]; then
+    xdotool key XF86AudioPlay
   fi
 
   # Play a sound (adjust the path to your sound file)
@@ -29,8 +33,8 @@ while true; do
   sleep $wait_period
 
   # Resume
-  if [ "$media_status" == "Playing" ]; then
-    playerctl play
+  if [ "$media_status" -eq 1 ]; then
+    xdotool key XF86AudioPlay
   fi
 
   # Print how long the break was (for fun)
